c6e95deffd843a2eae668bfbc7c8074977a199e9
bijoy <uzumakibijoy2017@gmail.com>
2022-01-11 21:20:37 +0600

    add hover text

--- function.js
+++ function.js
@@ -1,10 +1,13 @@
 let circle =document.querySelector("#circle");
+let text= document.querySelector(".text");
 
 let hover =document.querySelector(".hover");
 
 circle.addEventListener('mouseover', function(){
     if(!hover){
         circle.classList.add("hover")
+          text.innerHTML="Hey Sup :)"
+        
     }
 
 })       
@@ -12,6 +15,8 @@ circle.addEventListener('mouseover', function(){
 circle.addEventListener('mouseout', function(){
     if(circle.classList.contains("hover")){
         circle.classList.remove("hover")
+          text.innerHTML="Thanks for hovering"
+      
     }
 
 })       
